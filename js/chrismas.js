17704339249bd841bcf27bca8278c9d2b265e5c9
付科举 <92004577+fukeju@users.noreply.github.com>
2023-10-31 22:13:13 +0800

    Create chrismas.js

---
+++ js/chrismas.js
@@ -0,0 +1,13 @@
+var srcnaem = ["chrismas/christmas1/Event23-Christina-lv1_touch.webm", "chrismas/christmas1/Event23-Christina-lv1_finish.webm",
+    "chrismas/christmas1/Event23-Christina-lv2_idle.webm", "chrismas/christmas1/Event23-Christina-lv2_touch.webm", "chrismas/christmas1/Event23-Christina-lv2_finish.webm",
+    "chrismas/christmas1/Event23-Christina-lv3_idle.webm", "chrismas/christmas1/Event23-Christina-lv3_touch.webm", "chrismas/christmas1/Event23-Christina-lv3_finish.webm",
+    "chrismas/christmas1/Event23-Christina-lv4_idle.webm", "chrismas/christmas1/Event23-Christina-lv4_touch.webm", "chrismas/christmas1/Event23-Christina-lv4_finish.webm",
+    "chrismas/christmas1/Event23-Christina-lv5_idle.webm", "chrismas/christmas1/Event23-Christina-lv5_touch.webm", "chrismas/christmas1/Event23-Christina-end.webm"]
+var index = 0;
+function next() {
+    if (index <= srcnaem.length - 1) {
+        document.getElementById("christmas").src = srcnaem[index];
+        index++;
+    }
+}
+
