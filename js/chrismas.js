a622e6b8f4cd7066edce76474e2e36f32d372b7f
付科举 <92004577+fukeju@users.noreply.github.com>
2023-10-31 22:32:17 +0800

    Update chrismas.js

--- js/chrismas.js
+++ js/chrismas.js
@@ -1,8 +1,8 @@
-var srcnaem = ["chrismas/christmas1/Event23-Christina-lv1_touch.webm", "chrismas/christmas1/Event23-Christina-lv1_finish.webm",
-    "chrismas/christmas1/Event23-Christina-lv2_idle.webm", "chrismas/christmas1/Event23-Christina-lv2_touch.webm", "chrismas/christmas1/Event23-Christina-lv2_finish.webm",
-    "chrismas/christmas1/Event23-Christina-lv3_idle.webm", "chrismas/christmas1/Event23-Christina-lv3_touch.webm", "chrismas/christmas1/Event23-Christina-lv3_finish.webm",
-    "chrismas/christmas1/Event23-Christina-lv4_idle.webm", "chrismas/christmas1/Event23-Christina-lv4_touch.webm", "chrismas/christmas1/Event23-Christina-lv4_finish.webm",
-    "chrismas/christmas1/Event23-Christina-lv5_idle.webm", "chrismas/christmas1/Event23-Christina-lv5_touch.webm", "chrismas/christmas1/Event23-Christina-end.webm"]
+var srcnaem = ["chrismas/Christmas1/Event23-Christina-lv1_touch.webm", "chrismas/Christmas1/Event23-Christina-lv1_finish.webm",
+    "chrismas/Christmas1/Event23-Christina-lv2_idle.webm", "chrismas/Christmas1/Event23-Christina-lv2_touch.webm", "chrismas/Christmas1/Event23-Christina-lv2_finish.webm",
+    "chrismas/Christmas1/Event23-Christina-lv3_idle.webm", "chrismas/Christmas1/Event23-Christina-lv3_touch.webm", "chrismas/Christmas1/Event23-Christina-lv3_finish.webm",
+    "chrismas/Christmas1/Event23-Christina-lv4_idle.webm", "chrismas/Christmas1/Event23-Christina-lv4_touch.webm", "chrismas/Christmas1/Event23-Christina-lv4_finish.webm",
+    "chrismas/Christmas1/Event23-Christina-lv5_idle.webm", "chrismas/Christmas1/Event23-Christina-lv5_touch.webm", "chrismas/Christmas1/Event23-Christina-end.webm"]
 var index = 0;
 function next() {
     if (index <= srcnaem.length - 1) {
