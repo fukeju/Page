b92780295dc9b542b45f35a8a21a92ad8a79772c
付科举 <92004577+fukeju@users.noreply.github.com>
2023-11-01 21:39:56 +0800

    Update chrismas.js

--- js/chrismas.js
+++ js/chrismas.js
@@ -1,13 +1,9 @@
-var srcnaem = ["chrismas/Christmas1/Event23-Christina-lv1_touch.webm", "chrismas/Christmas1/Event23-Christina-lv1_finish.webm",
-    "chrismas/Christmas1/Event23-Christina-lv2_idle.webm", "chrismas/Christmas1/Event23-Christina-lv2_touch.webm", "chrismas/Christmas1/Event23-Christina-lv2_finish.webm",
-    "chrismas/Christmas1/Event23-Christina-lv3_idle.webm", "chrismas/Christmas1/Event23-Christina-lv3_touch.webm", "chrismas/Christmas1/Event23-Christina-lv3_finish.webm",
-    "chrismas/Christmas1/Event23-Christina-lv4_idle.webm", "chrismas/Christmas1/Event23-Christina-lv4_touch.webm", "chrismas/Christmas1/Event23-Christina-lv4_finish.webm",
-    "chrismas/Christmas1/Event23-Christina-lv5_idle.webm", "chrismas/Christmas1/Event23-Christina-lv5_touch.webm", "chrismas/Christmas1/Event23-Christina-end.webm"]
-var index = 0;
-function next() {
-    if (index <= srcnaem.length - 1) {
-        document.getElementById("christmas").src = srcnaem[index];
-        index++;
-    }
+
+
+var textclound = document.getElementById("textclound");
+
+function closetextclound() {
+    textclound.style.display = "none";
 }
+
 
